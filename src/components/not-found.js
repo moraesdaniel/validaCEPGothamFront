50444f05bf3368fea97f0c7e995c54bdafacc719
Daniel Moraes <daniel.moraes@viasoft.com.br>
2020-10-02 16:41:00 -0300

    Improving layout of the not-found page

--- src/components/not-found.js
+++ src/components/not-found.js
@@ -5,9 +5,10 @@ class NotFound extends Component {
     render() {
         return (
             <div className="container">
+                <h3>Ops! Página não encontrada!</h3>
+                <h5>Escolha uma das páginas abaixo:</h5>
                 <Link to="/">Adicionar CEP</Link> <br />
                 <Link to="/zip-code-list">Lista de CEP's</Link>
-                <h3>Ops! Página não encontrada!</h3>
             </div>
         );
     }
